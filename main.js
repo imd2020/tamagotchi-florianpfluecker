e0f7be91ae0f084b628e3178a26bde26787d8bac
florianpfluecker <73837515+florianpfluecker@users.noreply.github.com>
2021-05-26 19:24:54 +0200

    assets

---
+++ main.js
@@ -0,0 +1,37 @@
+//YOUTH
+youth = loadImage("./CC_T_YouthBody.png");
+youthAngry = loadImage("./CC_T_YouthBody-AngryEYES.png");
+youthHappy = loadImage("CC_T_YouthBody-HappyEYES.png");
+youthSad = loadImage("CC_T_YouthBody-SadEYES.png");
+//CHILD
+child = loadImage("CC_T_ChildBody.png");
+childSad = loadImage("CC_T_ChildBody-SADEyes.png");
+childHappy = loadImage("CC_T_ChildBody-HappyEYES.png");
+//BABY
+baby = loadImage("CC_T_ BabyBody.png");
+babyHappy = loadImage("CC_T_BabyBody-EyesHAPPY.png");
+babySad = loadImage("CC_T_BabyBody-EyesSAD.png");
+//EGG
+egg = loadImage("CC_T_Egg.png");
+eggCrack = loadImage("CC_T_Egg-Crack.png ");
+eggCracked = loadImage("CC_T_Egg-Cracked.png");
+
+function draw() {
+  //YOUTH
+  image(youth, 300, 100, 352, 518);
+  //image(youthAngry, 205, 145, 112, 156);
+  image(youthHappy, 405, 140, 112, 141);
+  //image(youthSad, 205, 140, 112, 136);
+  //CHILD
+  //image(child, 0, 290, 506, 322);
+  //image(childSad, 307, 347, 123, 86);
+  //image(childHappy, 307, 347, 123, 86);
+  //BABY
+  //image(baby, 170, 440, 385, 176);
+  //image(babyHappy, 340, 475, 55, 36);
+  //image(babySad, 338, 473, 60, 39);
+  //EGG
+  //image(egg, 100, 100, 141, 203);
+  //image(eggCrack, 300, 100, 141, 203);
+  //image(eggCracked, 500, 100, 141, 203);
+}
